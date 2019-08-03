9ac8178bf49f4e4cb272387b75ebae20eb632309
Marco Porcho <maporcho@gmail.com>
2019-08-03 14:41:56 -0300

    JSON Parsing and Dynamic Types

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -11,6 +13,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     super.initState();
+
     getLocation();
   }
 
@@ -25,6 +28,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
         'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
     if (response.statusCode == 200) {
       String data = response.body;
+
+      var decodedData = jsonDecode(data);
+
+      var temperature = decodedData['main']['temp'];
+      var condition_number = decodedData['weather'][0]['id'];
+      var city = decodedData['name'];
+
+      print(temperature);
+      print(condition_number);
+      print(city);
     } else {
       print(response.statusCode);
     }
